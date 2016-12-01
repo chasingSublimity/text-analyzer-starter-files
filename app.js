f65108378055205d4664d023bfd36e89365be89e
chasingSublimity <blakemsager@gmail.com>
2016-12-01 17:56:26 -0600

    initial commit and psuedocode

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // your code here!
+
+// Get Input, place in variable
+
+
+// Compute total word count
+
+// Compute unique word count
+
+// Compute average word length in characters
+
+// Compute average sentence length in characters
+
+// Write function that alters the text of a given <dd></dd> with 
+// the results of the compute functions above
