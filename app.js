e1f7df47f4212bed9b6a5830eb6122283c5bb979
chasingSublimity <blakemsager@gmail.com>
2016-12-01 23:09:42 -0600

    added buggy compute functions

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-$(document).ready(function() {
+// function for appending values to <dd></dd>'s
+var appendValue = function(jsClass, num) {
+	$(jsClass).text(num);
+	$(".hidden").removeClass("hidden");
+};
 
-	console.log("ready!")
-// Get Input, remove punctuation and return string 
-// as array of lower case letters
+// function for computing and displaying word count
+var computeWordCount = function (array) {
+	appendValue(".js-word-count", array.length);
+}
+// function for computing and displaying unique word count
+var computeUniqueCount = function(array) {
+	var results = [];
+	for (i=0; i < array.length; i++) {
+		if (!results.includes(array[i])) {
+			results.push(array[i]);
+		};
+	};
+	appendValue(".js-unique-word-count", results.length);
+};
+
+// function for computing and displaying average word length in characters
+var computeWordLength = function (array) {
+	var sum = 0;
+	for (i=0; i < array.length; i++) {
+		sum += array[i].length;
+	};
+	var avgWordLength = sum / array.length;
+	appendValue(".js-average-word-length", avgWordLength);
+};
+
+// function for computing and displaying average sentence length in characters
+var computeSentenceLength = function(array1, array2) {
+	var sentenceAvg = array1.length / array2.length;
+	appendValue(".js-average-sentence-length", sentenceAvg);
+}
+$(document).ready(function() {
 	
+// Get Input, remove punctuation and return string as array of lower case letters
 	$('form').submit(function(event) {
 		event.preventDefault();
-		var text = $('#user-text').val().toLowerCase().split(" ");
-		console.log(text);
-	});
+		var words = $('#user-text').val().toLowerCase().split(' ');
+		var characters = $('#user-text').val().toLowerCase().split('').filter(function(el) {return el.length != 0});
+		var sentences = $('#user-text').val().toLowerCase().split('.').filter(function(el) {return el.length != 0});
+		console.log(words);
 
-// Compute total word count
-
-// Compute unique word count
-
-// Compute average word length in characters
+// Analytic functions
+		computeWordCount(words);
+		computeUniqueCount(words);
+		computeWordLength(words);
+		computeSentenceLength(words, sentences);
 
 // Compute average sentence length in characters
 
-// Write function that alters the text of a given <dd></dd> with 
-// the results of the compute functions above
-
+	});
 });
 
 
