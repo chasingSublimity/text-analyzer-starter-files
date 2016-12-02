00d7a31883a32e59ead96766787a02c57d899501
chasingSublimity <blakemsager@gmail.com>
2016-12-02 10:32:48 -0600

    add toFixed() function to avg functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// function for appending values to <dd></dd>'s
+// function for appending values to <dd></dd>
 var appendValue = function(jsClass, num) {
 	$(jsClass).text(num);
 	$(".hidden").removeClass("hidden");
@@ -6,7 +6,7 @@ var appendValue = function(jsClass, num) {
 
 // function for computing and displaying word count
 var computeWordCount = function (array) {
-	appendValue(".js-word-count", array.length);
+	appendValue(".js-word-count", (array.length-1));
 }
 // function for computing and displaying unique word count
 var computeUniqueCount = function(array) {
@@ -25,13 +25,13 @@ var computeWordLength = function (array) {
 	for (i=0; i < array.length; i++) {
 		sum += array[i].length;
 	};
-	var avgWordLength = sum / array.length;
+	var avgWordLength = (sum / array.length).toFixed(2);
 	appendValue(".js-average-word-length", avgWordLength);
 };
 
 // function for computing and displaying average sentence length in characters
 var computeSentenceLength = function(array1, array2) {
-	var sentenceAvg = array1.length / array2.length;
+	var sentenceAvg = (array1.length / array2.length).toFixed(2);
 	appendValue(".js-average-sentence-length", sentenceAvg);
 }
 $(document).ready(function() {
@@ -39,7 +39,7 @@ $(document).ready(function() {
 // Get Input, remove punctuation and return string as array of lower case letters
 	$('form').submit(function(event) {
 		event.preventDefault();
-		var words = $('#user-text').val().toLowerCase().split(/[ ,!."';+:-]/).filter(function(el) {return el.length != 0});
+		var words = $('#user-text').val().toLowerCase().split(/[ ,!?$."';+:-]/).filter(function(el) {return el.length != 0});
 		var characters = $('#user-text').val().toLowerCase().split('').filter(function(el) {return el.length != 0});
 		var sentences = $('#user-text').val().toLowerCase().split('.').filter(function(el) {return el.length != 0});
 		console.log(words);
